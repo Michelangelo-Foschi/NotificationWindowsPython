e247b1df505306ef2140f96e82d9dfefcba9e431
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-11-21 11:38:35 +0400

    Add code

--- Code.py
+++ Code.py
@@ -1,1 +1,9 @@
+from win10toast import ToastNotifier
 
+toaster = ToastNotifier()
+
+toaster.show_toast("New Notification!", "Alerting You Right now!", threaded=True, icon_path=None, duration=5)
+
+import time
+while toaster.notification_active():
+    time.sleep(0.1)
